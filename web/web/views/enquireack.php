694886b5649ea50f84473fd677992240234d0af8
niteshp27 <niteshp27@gmail.com>
2016-01-17 17:59:00 +0530

    created a seperate contact form instead of having it on same page. Created new twig pages and moved them under pages folder inside views folder

---
+++ web/web/views/enquireack.php
@@ -0,0 +1,3 @@
+<h1>Thank You</h1>
+Thank you for getting in touch. If a response is required we will get back to you as soon as we can..
+
---
+++ web/web/views/enquireunack.php
@@ -0,0 +1,2 @@
+<h1>Oops!</h1>
+Sorry, there was a problem sending your message.123
